b7d2cdab79675311dd7d236488d366387b8b6fd4
luiza318 <luiza_l_santos@estudante.sesisenai.org.br>
2026-03-30 11:15:53 -0300

    post criando

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,1 +1,17 @@
 const repo = require ("../repositories/post.repo");
+
+function createPost (req, res){
+    let body = req.body;
+    repo.insertPost(body)
+}
+
+module.exports = {createPost}
+
+/*{
+    "conteudo": 1,
+    "descripition" : "descrição do post",
+    "midia" : [
+        {"url" : "/midia/img/image1.jpg"},
+        {"url" : "/midia/video/video1.mp4"}
+    ]
+}*/
--- src/repositories/post.repo.js
+++ src/repositories/post.repo.js
@@ -1,5 +1,7 @@
 const db = require("../config/db");
 
-async function createPost() {
-    
+async function insertPost(body) {
+    let sql = 'insert into posts (user_id, descricao) values (?,?)';
 }
+
+module.exports = {insertPost}
