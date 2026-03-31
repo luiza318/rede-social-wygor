7377307015ca65bd841cc93624637cfdeefb03f6
luiza318 <luiza_l_santos@estudante.sesisenai.org.br>
2026-03-31 11:35:13 -0300

    hh

--- src/repositories/post.repo.js
+++ src/repositories/post.repo.js
@@ -1,10 +1,10 @@
 const db = require("../config/db");
 
 async function insertPost(body) {
-    let {user_id, conteudo, mideas} = body;
+    let {user_id, conteudo, midias} = body;
 
     let sql = 'insert into posts (user_id, conteudo) values (?,?)';
-        const [result] = await db.query(
+    const [result] = await db.query(
         sql,
         [user_id, conteudo]
     );
@@ -12,13 +12,14 @@ async function insertPost(body) {
     let post_id = result.insertId;
 
     // inserir mideas na tabela
-    for (m in mideas){
-        insertMidias(post_id ,m.url);
+    for (m of midias){
+        console.log(m.url)
+        await insertMidias(post_id, m.url);
     }
 }
 
 async function insertMidias(post_id, url) {
-    let sql =  'insert into mideas (post_id, url) values (?,?)';
+    let sql =  'insert into midias (post_id, url) values (?,?)';
     const [result] = await db.query(
         sql,
         {post_id, url}
